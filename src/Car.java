c7e05178b82ed8f27f6a1b4d83d4e9cb6fbf2026
ThePussyCat <35796281+ThePussyCat@users.noreply.github.com>
2019-11-06 16:11:51 +0100

    t

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,42 @@
-public abstract class Car {
+import java.awt.*;
 
+public abstract class Car {
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    protected String modelName; // The car model name
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    public abstract void incrementSpeed(double amount);
+
+    public abstract void  decrementSpeed(double amount);
 }
