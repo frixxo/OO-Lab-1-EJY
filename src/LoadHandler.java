c06d741d04a9604f3cab5748bfaffdaad8bcf558
kusuuuuuu <nanmu.w4@gmail.com>
2019-11-08 13:22:40 +0100

    fix later

--- src/LoadHandler.java
+++ src/LoadHandler.java
@@ -1,18 +1,44 @@
-import java.util.List;
+import java.awt.*;
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class LoadHandler {
-    List<Car> carList;
+    Deque<Car> carList;
+    Vehicle transport;
+    private final int MAX_CAR_LOAD;
+    private final int MAX_LOAD_DISTANCE;
+    private boolean ramp;
 
-    void add(){
-
+    public LoadHandler(Vehicle transport, int maxCarLoad, int maxLoadDistance){
+        carList = new LinkedList<>();
+        ramp = false;
+        MAX_CAR_LOAD = maxCarLoad;
+        MAX_LOAD_DISTANCE = maxLoadDistance;
+        this.transport = transport;
     }
 
-    Object pull(){
 
+    public void load(Car car) throws Exception {
+        if (ramp && carList.size() < MAX_CAR_LOAD && isCarBehind(car)) {
+            carList.push(car);
+            return;
+        } throw new Exception("Cannot load the car!");
     }
 
-    Object poll(){
-
+    public Car release() throws Exception {
+        if (ramp){
+            Car car = this.car.pop();
+            car.position.setLocation((int)(transport.position.x + -10 * transport.direction.getX()),
+                    (int)(transport.position.y + -10 * transport.direction.getY()));
+            return car;
+        } throw new Exception("Ramp not lowered!");
     }
 
+    private boolean isCarBehind(Car car){
+        int dX = (int)(transport.position.x + -MAX_LOAD_DISTANCE * transport.direction.getX());
+        int dY = (int)(transport.position.y + -MAX_LOAD_DISTANCE * transport.direction.getY());
+
+        Rectangle rec = new Rectangle(dX-10, dY-10, dX+10, dY+10);
+        return rec.contains(car.getPosition());
+    }
 }
