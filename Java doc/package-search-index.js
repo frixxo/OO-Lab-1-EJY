8960bf3b52483447d486b43138b3bc870fba47cc
joel olausson <joel.olaus@gmail.com>
2019-11-21 20:21:34 +0100

    smått o gott javadox

--- Java doc/package-search-index.js
+++ Java doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"LastHandle"},{"l":"Movables"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Flak"},{"l":"LastHandle"},{"l":"Movables"}]
--- src/Flak/IFlak.java
+++ src/Flak/IFlak.java
@@ -4,9 +4,11 @@ public interface IFlak {
     boolean lowerRamp();
     boolean raiseRamp();
 
-    /** the normal state of the flak, and when it is supposed to be able to move*/
+    /** the normal state of the flak, and when it is supposed to be able to move
+     * @return if in normalstate*/
     boolean normalState();
-    /** Defines when the flak is supposed to be able to move*/
+    /** Defines when the flak is supposed to be able to move
+     * @return if in normalstate*/
     boolean loadState();
 
     double getAngle();
