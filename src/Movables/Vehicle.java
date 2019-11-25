ac13f139d00234e84fffe46d91715bf4998ca327
joel olausson <joel.olaus@gmail.com>
2019-11-25 15:17:40 +0100

    tweaked rotation method

--- src/Movables/Vehicle.java
+++ src/Movables/Vehicle.java
@@ -116,7 +116,7 @@ abstract public class Vehicle implements Movable{
      * @return if the vehicle is moving
      * */
     protected boolean isMoving(){
-        return (Math.abs(currentSpeed)-0.01>0);
+        return (Math.abs(currentSpeed)-0.001>0);
     }
     //endregion
 
@@ -124,8 +124,8 @@ abstract public class Vehicle implements Movable{
     /** move car ahead*/
     public void move(){
         if(!IsLoaded){
-            if(currentSpeed*fixLowSpeed<0.5)fixLowSpeed++;            //Helps when speed is low so that the car moves every other frame
-            else fixLowSpeed=1;
+            if(currentSpeed*fixLowSpeed<0.5)fixLowSpeed++;            //Helps when speed is low so that the car moves every other frame instead of not moving at all
+            else { fixLowSpeed=1;}
             position.translate((int)Math.round(direction.getX()*currentSpeed*fixLowSpeed),(int)Math.round(direction.getY()*currentSpeed*fixLowSpeed));
         }
     }
@@ -136,16 +136,15 @@ abstract public class Vehicle implements Movable{
         rotate(360-turnAngle);
     }
 
-    /** calculates the rotation of the car when using turn methods (Origo is top left so we had to flip the rotation using -x,-y)
+    /** calculates the rotation of the car when using turn methods (Origo is top left so we had to flip the rotation using -y)
      * @param angle how much to turn
      * */
     private void rotate (double angle)
     {
-        if(this.getClass()==Volvo240.class)System.out.println("" +direction);
         angle = angle*(Math.PI/180);
         double x = direction.getX();
         double y = direction.getY();
-        direction.setLocation(-(x*Math.cos(angle) - y*Math.sin(angle)), -(x*Math.sin(angle) + y*Math.cos(angle)));
+        direction.setLocation(x*Math.cos(angle) - (-y)*Math.sin(angle), -(x*Math.sin(angle) + (-y)*Math.cos(angle)));
     }
 
     /** makes sure that the car is not slightly outside of frame when hitting a wall and puts it back on the border
