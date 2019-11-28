c8e20592e1f42533d4f201c9fb09509e53b7a61c
joel olausson <joel.olaus@gmail.com>
2019-11-28 16:16:01 +0100

    asd

--- src/Fuctionality/TrimmedMotor.java
+++ src/Fuctionality/TrimmedMotor.java
@@ -3,8 +3,8 @@ package Fuctionality;
 public class TrimmedMotor extends NormalMotor{
     protected double trimFactor;
 
-    public TrimmedMotor(double power,double trimFactor) {
-        super(power);
+    public TrimmedMotor(double power,double trimFactor,DriveHandler driveHandler) {
+        super(power,driveHandler);
         this.trimFactor=trimFactor;
     }
 
