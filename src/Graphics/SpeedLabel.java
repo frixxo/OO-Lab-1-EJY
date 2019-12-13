c665e26a25fb90a50026f1398f44cf53c437149f
kusuuuuuu <nanmu.w4@gmail.com>
2019-12-13 12:04:14 +0100

    Update SpeedLabel.java

--- src/Graphics/SpeedLabel.java
+++ src/Graphics/SpeedLabel.java
@@ -4,37 +4,35 @@ import Systems.CarSimulator;
 import Systems.Observer.Observer;
 import WorldObjects.InterfaceHierarchy.WorldObjectView;
 import WorldObjects.Objects.Drivable;
-import WorldObjects.Objects.WorldObject;
+
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
 
 public class SpeedLabel extends JLabel implements Observer {
-    private List<WorldObjectView> vehicles;
     private CarSimulator cs;
-    JLabel lable;
-    StringBuilder sb = new StringBuilder();
+    private JLabel label;
+    private StringBuilder sb = new StringBuilder();
 
     public SpeedLabel(CarSimulator cs, Dimension size){
         super();
-        this.vehicles = cs.getVehicles();
-        update();
-        this.cs=cs;
-        lable=new JLabel();
+
+        this.cs = cs;
+
+        label = new JLabel();
+        this.add(label);
         this.setPreferredSize(size);
+
+        update();
     }
-    
+
     @Override
-    public void update(){
-        StringBuilder sb = new StringBuilder();
+    public void update() {
         for (WorldObjectView vehicle : cs.getVehicles()){
             if(vehicle instanceof Drivable){
                 sb.append(vehicle.getType()).append(": ").append(((Drivable)vehicle).getCurrentSpeed()).append("; ");
             }
         }
-        lable.setText(sb.toString());
-        sb.delete(0,sb.length());
-        this.add(lable);
+        label.setText(sb.toString());
+        sb.delete(0, sb.length());
     }
-
 }
